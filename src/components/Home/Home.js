69108399689390d7f791dc56bd1cdfc3fca30aa2
svetoslavmanolov <svn.manolov@gmail.com>
2023-05-16 18:23:26 +0300

    design changes on home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,17 +7,7 @@ export const Home = () => {
         <section className="slider_section">
             <div className="slider_container">
                 <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                    {/* <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active">
-                            01
-                        </li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1">
-                            02
-                        </li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2">
-                            03
-                        </li>
-                    </ol> */}
+                   
                     <div className="carousel-inner">
                         <div className="carousel-item active">
                             <div className="container-fluid">
